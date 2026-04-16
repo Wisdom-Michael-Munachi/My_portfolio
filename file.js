c733d293c329f05776c39fba3bacedbe3493acca
Wisdom-Michael-Munachi <wisdom.1376@gmail.com>
2026-04-17 00:19:27 +0100

    Update file.js

--- file.js
+++ file.js
@@ -1,7 +1,1 @@
-function time(){
-var year=new Date()
-var current_year=year.getFullYear()
- document.getElementById("year").textContent=current_year   
-}
-setInterval(time, 1000);
-
+document.getElementById("year").textContent=new Date().getFullYear()
